2f541d5d393d6a42c6e04dbb7a35702860317413
Pelle <joaquinpellegrino@gmail.com>
2018-05-31 16:39:19 -0300

    cambios varios

--- Hospital/Hospital.cs
+++ Hospital/Hospital.cs
@@ -21,98 +21,12 @@ namespace Hospital
             this.farmacia = farmacia;
         }
 
-        private bool IngresoMenuADM()
-        {
-            string user, pass;
-
-            do {
-                Console.WriteLine("Ingrese el nombre de usuario: ");
-                user = Console.ReadLine();
-                Console.WriteLine("Ingrese pass: ");
-                pass = Console.ReadLine();
-            } while (!admin.VerificarUser(user) || !admin.VerificarPass(pass));
-
-            Console.WriteLine();
-
-            return true;
-        }
-
-        private void MenuADM()
-        {
-            char opc;
-
-            do {
-                Console.WriteLine("1> Crear Paciente\n2> Borrar Paciente\n3> Modificar Paciente\n4> Consultar Paciente\n5> Salir");
-                opc = char.Parse(Console.ReadLine());
-
-                if (opc == '1')
-                    admin.AgregarPaciente(ref pacientes, ref medicos);
-
-                if (opc == '2')
-                    admin.BorrarPaciente(ref pacientes);
-
-                if (opc == '3')
-                    admin.ModificarPaciente(ref pacientes);
-
-                if (opc == '4')
-                    admin.ConsultarPaciente(pacientes);
-            } while (opc != '5');
-        }
-
-        private int BuscarMedico(string dni)
-        {
-            for (int i = 0; i < medicos.Length; i++)
-            {
-                if (medicos[i].DNI.Equals(dni))
-                    return i;
-            }
-
-            return -1;
-        }
-
         private void ActualizarPaciente(Paciente paciente)
         {
             for (int i = 0; i < pacientes.Count; i++)
             {
                 if (pacientes[i].Equals(paciente))
                     pacientes[i] = paciente;
-            }
-        }
-
-        private void MenuMedico()
-        {
-            string dni;
-            int i;
-            Paciente paciente;
-
-            Console.WriteLine("Ingrese dni del medico: ");
-            dni = Console.ReadLine();
-
-            i = BuscarMedico(dni);
-
-            if (i != -1) {
-                paciente = medicos[i].AtenderPaciente(medicos[i].ConsultarTurnos());
-
-                ActualizarPaciente(paciente);
-            }
-        }
-
-        private void MenuPaciente()
-        {
-            string dni;
-            int i;
-
-            Console.WriteLine("Ingrese dni del paciente que quiere solicitar el turno: ");
-            dni = Console.ReadLine();
-
-            i = BuscarPaciente(dni);
-
-            if (i == -1)
-                Console.WriteLine("No se encontro el paciente.");
-            else {
-                pacientes[i].SolicitarTurno(true);
-
-                AsignarTurnos(CrearTurno());
             }
         }
 
@@ -124,11 +38,17 @@ namespace Hospital
                 Console.WriteLine("1> Pacientes\n2> Medicos\n3> Farmacia\n4> Admin\n5> Salir");
                 opc = char.Parse(Console.ReadLine());
 
-                if (opc == '1')
-                    MenuPaciente();
+                if (opc == '1') {
+                    Menu.MenuPaciente(ref pacientes);
 
-                if (opc == '2')
-                    MenuMedico(); 
+                    AsignarTurnos(CrearTurno());
+                }
+
+                if (opc == '2') {
+                    var paciente = Menu.MenuMedico(ref medicos);
+
+                    ActualizarPaciente(paciente);
+                }
 
                 if (opc == '3')
                 {
@@ -136,23 +56,11 @@ namespace Hospital
                 }
 
                 if (opc == '4') {
-                    if (IngresoMenuADM())
-                        MenuADM();
-
+                    if (Menu.IngresoMenuADM(admin))
+                        Menu.MenuADM(admin, ref pacientes, medicos);
                 }
             } while (opc != '5');
 
-        }
-
-        private int BuscarPaciente(string dni)
-        {
-            for (int i = 0; i < pacientes.Count; i++)
-            {
-                if (pacientes[i].DNI.Equals(dni))
-                    return i;
-            }
-
-            return -1;
         }
 
         private void AsignarTurnos(Turno turno)
--- Hospital/Menu.cs
+++ Hospital/Menu.cs
@@ -8,7 +8,101 @@ namespace Hospital
 {
     static class Menu
     {
+        public static bool IngresoMenuADM(Administrador admin)
+        {
+            string user, pass;
 
+            do {
+                Console.WriteLine("Ingrese el nombre de usuario: ");
+                user = Console.ReadLine();
+                Console.WriteLine("Ingrese pass: ");
+                pass = Console.ReadLine();
+            } while (!admin.VerificarUser(user) || !admin.VerificarPass(pass));
+
+            Console.WriteLine();
+
+            return true;
+        }
+
+        public static void MenuADM(Administrador admin, ref List<Paciente> pacientes, Medico[] medicos)
+        {
+            char opc;
+
+            do {
+                Console.WriteLine("1> Crear Paciente\n2> Borrar Paciente\n3> Modificar Paciente\n4> Consultar Paciente\n5> Salir");
+                opc = char.Parse(Console.ReadLine());
+
+                if (opc == '1')
+                    admin.AgregarPaciente(ref pacientes, ref medicos);
+
+                if (opc == '2')
+                    admin.BorrarPaciente(ref pacientes);
+
+                if (opc == '3')
+                    admin.ModificarPaciente(ref pacientes);
+
+                if (opc == '4')
+                    admin.ConsultarPaciente(pacientes);
+            } while (opc != '5');
+        }
+
+        private static  int BuscarPaciente(string dni, List<Paciente> pacientes)
+        {
+            for (int i = 0; i < pacientes.Count; i++)
+            {
+                if (pacientes[i].DNI.Equals(dni))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public static void MenuPaciente(ref List<Paciente> pacientes)
+        {
+            string dni;
+            int i;
+
+            Console.WriteLine("Ingrese dni del paciente que quiere solicitar el turno: ");
+            dni = Console.ReadLine();
+
+            i = BuscarPaciente(dni, pacientes);
+
+            if (i == -1)
+                Console.WriteLine("No se encontro el paciente.");
+            else
+                pacientes[i].SolicitarTurno(true);
+        }
+
+        private static int BuscarMedico(string dni, Medico[] medicos)
+        {
+            for (int i = 0; i < medicos.Length; i++)
+            {
+                if (medicos[i].DNI.Equals(dni))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public static Paciente MenuMedico(ref Medico[] medicos)
+        {
+            // TODO probar que esto funcione
+
+            string dni;
+            int i;
+            Paciente paciente = null;
+
+            Console.WriteLine("Ingrese dni del medico: ");
+            dni = Console.ReadLine();
+
+            i = BuscarMedico(dni, medicos);
+
+            if (i != -1) {
+                paciente = medicos[i].AtenderPaciente(medicos[i].ConsultarTurnos());
+            }
+
+            return paciente;
+        }
 
     }
 }
